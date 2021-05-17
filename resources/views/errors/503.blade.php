918c44345287f2ea1560769a8534b5a4b842a999
williamning <61674789+williamning@users.noreply.github.com>
2021-05-17 10:30:42 +0800

    *  Modefy .gitignore [Add .vscode .history] *  Modefy timezone and locale [Asia/Shanghai, zh-CN] *  Up/donw maintenance mode [Add 503 Service Unavaiable custom view]

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,7 @@
+<h2>自定义服务不可用视图[Custom View]</h2>
+<span style="color: red">503 Service Unavailable</span> comes from<span style="color: green"> resources/views/errors/503.blade.php</span>
+<br>
+<br>
+<spann style="color: red">Note</span>
+<br>
+由于维护模式命令的执行需要几秒时间，你可以考虑使用 <a href="https://envoyer.io/" target="_blank">Envoyer</a> 实现零秒下线作为替代方案。
